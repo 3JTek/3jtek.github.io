e1a7ec2a1dca3552aea89c657dcc81eac8e43ff9
3jtechno <3j.techno@gmail.com>
2019-03-17 20:15:53 +0000

    Reduce slide width

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ let canScroll = true
 function init(){
 
   const slider = $('.slider')
-  const slideWidth = slider[0].scrollWidth / 3
+  const slideWidth = 0.835 * slider[0].scrollWidth / 3
+  // slider[0].scrollLeft = -0.01 * slider[0].scrollWidth / 3
   slider.bind('mousewheel DOMMouseScroll', scrollSlide)
 
   function scrollSlide(e){
