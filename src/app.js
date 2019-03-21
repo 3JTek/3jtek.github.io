247ea1bfab6fced4f7cfe2a0c7fea6030ab0d129
3jtechno <3j.techno@gmail.com>
2019-03-21 10:56:45 +0000

    Update to Coming Soon page

--- src/app.js
+++ src/app.js
@@ -5,13 +5,15 @@ let canScroll = true
 function init(){
 
   const slider = $('.slider')
+  //Calculate the width of one project slide
   const slideWidth = slider[0].scrollWidth / 3
-  // slider[0].scrollLeft = -0.01 * slider[0].scrollWidth / 3
+
+  //Event listener for 2 fingers type of scrolling (or mouseWheel)
   slider.bind('mousewheel DOMMouseScroll', scrollSlide)
 
   function scrollSlide(e){
+    //Calculate the horizontal scrolling
     const deltaX = e.originalEvent.deltaX
-
     //Prevent any normal scrolling
     if(deltaX) e.preventDefault()
     //Prevent sliding logic in mobile view
@@ -19,6 +21,8 @@ function init(){
     //Make sure the user wants to scroll horizontally
     if(Math.abs(deltaX) > 15 && canScroll) {
 
+      //Allows to only run the code below once every 600 ms just to avoid
+      //having the code run multiple time per second
       canScroll = false
 
       const direction = deltaX > 0 ? 1 : -1
